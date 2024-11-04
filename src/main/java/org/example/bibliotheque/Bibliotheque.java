0f25859f4fdbfe44e72929788ce4299681dd907b
johan guillen <sioupes34@gmail.com>
2024-11-04 18:08:59 +0100

    exo2

--- src/main/java/org/example/bibliotheque/Bibliotheque.java
+++ src/main/java/org/example/bibliotheque/Bibliotheque.java
@@ -1,4 +1,24 @@
 package org.example.bibliotheque;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Bibliotheque {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo-jpa2");
+        EntityManager em = emf.createEntityManager();
+        System.out.println(em);
+
+        Livre livre = new Livre(1);
+        em.persist(livre);
+        Livre l = em.find(Livre.class,1);
+        if (l != null) {
+            System.out.println(l.getId());
+        }
+
+        em.close();
+        emf.close();
+    }
+
 }
--- src/main/java/org/example/bibliotheque/Livre.java
+++ src/main/java/org/example/bibliotheque/Livre.java
@@ -1,4 +1,24 @@
 package org.example.bibliotheque;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="livre")
 public class Livre {
+    @Id
+    private Integer id;
+
+    public Livre(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * Getter
+     *
+     * @return id
+     */
+
+    public Integer getId() {
+        return id;
+    }
 }
